498940a52e7c96d510bbb11111eb22ae04177b54
Игорь Токарев <tokarev@id317.ru>
2018-11-04 13:50:52 +0500

    parameters processing was add

--- py_scanner/py_scanner.py
+++ py_scanner/py_scanner.py
@@ -1,12 +1,13 @@
 import logging, queue
 from .scanner_thread import ScannerThread
 class PyScanner:
-    def __init__(self, threads_num=1, host="127.0.0.1"):
+    def __init__(self, params_names={"-threads":5, "-ip": "127.0.0.1"}):
         # print("ok")
         # print(dir(queue))
+        params_names["-threads"]=int(params_names["-threads"])
         self.work_queue = queue.Queue(1)
         self.threads = []
-        for i in range(threads_num):
+        for i in range(params_names["-threads"]):
             thread = ScannerThread(i)
             self.threads.append(thread)
         for th in self.threads:
--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
 from py_scanner import py_scanner
 import sys
 print(sys.argv)
-scanner = py_scanner.PyScanner(5)
+args_names = {
+    "-ip": 0,
+    "-threads": 0
+}
+vals = []
+for key in args_names.keys():
+    args_names[key] = sys.argv.index(key) if key in sys.argv else -1
+    if args_names[key] != -1:
+        idx = sys.argv.index(key)+1
+        args_names[key] = sys.argv[idx] if idx < len(sys.argv) else -1
+
+print(args_names)
+scanner = py_scanner.PyScanner(args_names)
